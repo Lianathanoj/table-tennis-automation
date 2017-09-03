e46d54cc84abfa3f7f5229b49d66c4395083fc27
Jonathan Lian <jlian@gatech.edu>
2017-09-03 15:31:13 -0400

    added user input for determining number of groups and respective group counts

--- automation.py
+++ automation.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import httplib2
 import os
+import sys
 
 from pprint import pprint
 from apiclient import discovery
@@ -46,20 +47,44 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+def get_group_sizes():
+    try:
+        # find out how many groups there were
+        num_groups = int(input("How many groups were there?\n"))
+
+        # check if it was valid input
+        while not 1 <= int(num_groups) <= 4:
+            num_groups = int(input("How many groups were there?\n"))
+
+        # determine how many people were in each group and check for valid input
+        group_numbers = [int(input("How many people were in Group {}?\n".format(i)))
+                         for i in range(1, int(num_groups) + 1)]
+
+        # check for valid group size for first group
+        while not 4 <= group_numbers[0] <= 7:
+            group_numbers[0] = int(input("Group 1 needs to be between 4 and 7 people. Try again.\n"))
+
+        # if there is more than one group, check if the group sizes are between 4 and 6 for all groups besides group 1.
+        if len(group_numbers) > 0:
+            for i in range(1, len(group_numbers)):
+                while not 4 <= group_numbers[i] <= 6:
+                    group_numbers[i] = int(input('Group {} needs to be between 4 and 6 people. Try again.\n'.format(i + 1)))
+        return group_numbers
+    except ValueError:
+        print('You did not input a valid integer.')
+        sys.exit()
+
 def main():
+    # TODO: implement going backwards or redoing previous steps (probably use a heap)
+    # TODO: subdivide code into modular portions
+
+    group_numbers = get_group_sizes()
+
+    sys.exit()
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
-
-    # get JSON template of spreadsheet
-    # summary_request = service.spreadsheets().values().batchGet(
-    #     spreadsheetId=summary_template_id,
-    #     ranges=['A:A', 'B:B', 'C:C', 'D:D', 'E:E', 'F:F', 'G:G', 'H:H', 'I:I']
-    # )
-    # summary_response = summary_request.execute()
+    discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
+    service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
 
     # first we create an empty spreadsheet
     new_spreadsheet_request = service.spreadsheets().create(body={})
@@ -68,20 +93,19 @@ def main():
 
     # this is the id for the summary page template (currently called "SUMMARY TEMPLATE")
     # we start out with no formulas on this page to mitigate request errors
-    summary_template_id = '1C-FkQT2XNTaylUV28e-VbsD7FTPZwjyQb1kOwYubMMg'
-
-    body = {
+    template_id = '1C-FkQT2XNTaylUV28e-VbsD7FTPZwjyQb1kOwYubMMg'
+    destination_body = {
         # The ID of the spreadsheet to copy the sheet to.
         'destination_spreadsheet_id': new_spreadsheet_id
     }
 
     # now we copy the summary page template to the empty spreadsheet we created
     # note: if the sheet that you're trying to copy over references other sheets in its formula, copying may not work
-    copy_request = service.spreadsheets().sheets().copyTo(spreadsheetId=summary_template_id, sheetId=0, body=body)
+    copy_request = service.spreadsheets().sheets().copyTo(spreadsheetId=template_id, sheetId=0, body=destination_body)
     copy_response = copy_request.execute()
     copied_sheet_id = copy_response['sheetId']
 
-    # Now we need to clean up and remove the first sheet, rename the copied sheet, and rename the spreadsheet itselfs
+    # Now we need to clean up and remove the first sheet, rename the copied sheet, and rename the spreadsheet itself
     cleanup_body = {
         'requests': [
             {
